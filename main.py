28b86a5fe475db0331ae6d49d04f7d6b9535306b
Siri <ellabuddy2000@gmail.com>
2025-06-30 23:06:54 -0500

    Update main.py with fix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from typing import List
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
